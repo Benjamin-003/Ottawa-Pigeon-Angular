678ceadc007d257e54252bb4b4363725d17a5c7f
Benjamin Boissin <benjamin.boissin@modis.com>
2022-11-21 15:33:01 +0100

    task: implementation des sous menus

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  public items!:MenuItem[]
+  public items!: MenuItem[]
   ngOnInit() {
-    this.items = [{label:"Tarif",routerLink:"tarifs"},{label:"Investir", routerLink:"investissement"},{label:"Académie"},{label:"A propos de SVB Ind"},{label:"Des questions"}];
+    this.items = [{ label: "Tarif", routerLink: "tarifs" }, { label: "Investir", routerLink: "investissement" }, { label: "Académie" }, { label: "A propos de SVB Ind", items: [{ label: "À propos d'Ottawa Pigeon" }, { label: "Centre d'aide" }, { label: "Pourquoi nous choisir ?" }] }, { label: "Des questions" }];
   }
 }
