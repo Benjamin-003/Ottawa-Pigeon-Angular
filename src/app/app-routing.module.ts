201422d11349c8052efc535897000d64491a6219
Benjamin Boissin <benjamin.boissin@modiscloud.net>
2022-11-28 10:31:23 +0000

    Merged PR 7715: task: Correction du routage à la racine de l'application
    task: Correction du routage à la racine de l'application
    
    Related work items: #25753, #25754

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
+import { AccueilComponent } from './core/page/accueil/accueil.component';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{path: '', redirectTo: 'accueil', pathMatch: 'full' },
-{path: 'accueil', loadChildren: () => import('./core/core.module').then(m => m.CoreModule)},
-{path: 'investissement', loadChildren: () => import('./investissement/investissement.module').then(m => m.InvestissementModule)},
-{path: 'tarifs', loadChildren: () => import('./tarifs/tarifs.module').then(m => m.TarifsModule)},
-{path: 'authentification', loadChildren: () => import('./authentification/authentification.module').then(m => m.AuthentificationModule)},
-{path: 'information-societe', loadChildren: () => import('./information-societe/information-societe.module').then(m => m.InformationSocieteModule) }];
+const routes: Routes = [
+{ path: 'accueil', component: AccueilComponent },
+{ path: 'investissement', loadChildren: () => import('./investissement/investissement.module').then(m => m.InvestissementModule) },
+{ path: 'tarifs', loadChildren: () => import('./tarifs/tarifs.module').then(m => m.TarifsModule) },
+{ path: 'authentification', loadChildren: () => import('./authentification/authentification.module').then(m => m.AuthentificationModule) },
+{ path: 'information-societe', loadChildren: () => import('./information-societe/information-societe.module').then(m => m.InformationSocieteModule) },
+{ path: '', redirectTo: 'accueil', pathMatch: 'full' }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules})],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
