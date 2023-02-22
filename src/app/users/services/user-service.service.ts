8879f23a3685bcf8f45a4e28ec38f2bf5d0faba6
Joffrey PARET <joffrey.paret@modiscloud.net>
2023-02-22 11:10:41 +0100

    🎨 Format user service with prettier formatter.

--- src/app/users/services/user-service.service.ts
+++ src/app/users/services/user-service.service.ts
@@ -4,7 +4,7 @@ import { LoggedUser } from '../../authentification/Interfaces/logged-user.model'
 import { Token } from '../../authentification/Interfaces/token.model';
 import { User } from '../interfaces/user.model';
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable, BehaviorSubject } from 'rxjs';
 
@@ -12,17 +12,33 @@ const userEndpoint = `${environment.urlApi}/users`;
 const mailEndpoint = `${environment.urlApi}/mails`;
 const loginEndpoint = `${environment.urlApi}/tokens`;
 const USER_KEY = 'authentification-user';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UserService {
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {}
 
-  public currentLoggedUser = new BehaviorSubject<LoggedUser>({ id: 0, firstname: '' });
-  private _currentPersonalData$ = new BehaviorSubject<PersonalData>({ surname: '', firstname: '', birth_date: '', address: '', zip_code: '', city: '', country: '', mail: '', language_code: '', currency_code: '' })
+  public currentLoggedUser = new BehaviorSubject<LoggedUser>({
+    id: 0,
+    firstname: '',
+  });
+  private _currentPersonalData$ = new BehaviorSubject<PersonalData>({
+    surname: '',
+    firstname: '',
+    birth_date: '',
+    address: '',
+    zip_code: '',
+    city: '',
+    country: '',
+    mail: '',
+    language_code: '',
+    currency_code: '',
+  });
 
-  get token() { return sessionStorage.getItem("authentification-user") }
+  get token() {
+    return sessionStorage.getItem('authentification-user');
+  }
 
   get currentPersonalData$() {
     return this._currentPersonalData$.asObservable();
@@ -35,27 +51,34 @@ export class UserService {
 
   //Envoi d'un nouveau user vers le back
   updateUser(userToUpdate: PersonalData) {
-    return this.http.patch(userEndpoint + "/" + this.currentLoggedUser.value.id, userToUpdate).pipe(map(() => {
-      this.getUser(this.currentLoggedUser.value.id).subscribe()
-    })
-    );
+    return this.http
+      .patch(userEndpoint + '/' + this.currentLoggedUser.value.id, userToUpdate)
+      .pipe(
+        map(() => {
+          this.getUser(this.currentLoggedUser.value.id).subscribe();
+        })
+      );
   }
 
   //Obtenir les informations personnelles d'un utilisateur
   getUser(idUser: number): Observable<PersonalData> {
-    return this.http.get<PersonalData>(`${userEndpoint}/${idUser}`).pipe(map(
-      (personalData) => {
-        this._currentPersonalData$.next(personalData)
-        return personalData
-      }
-    ));
+    return this.http.get<PersonalData>(`${userEndpoint}/${idUser}`).pipe(
+      map((personalData) => {
+        this._currentPersonalData$.next(personalData);
+        return personalData;
+      })
+    );
   }
 
   //Verification des doublons d'email
   isMailTaken(mail: string) {
-    return this.http.head(`${mailEndpoint}/${mail}`, { observe: 'response' }).pipe(map(response => {
-      return response.status
-    }))
+    return this.http
+      .head(`${mailEndpoint}/${mail}`, { observe: 'response' })
+      .pipe(
+        map((response) => {
+          return response.status;
+        })
+      );
   }
 
   //Appel le back pour la connexion
@@ -71,12 +94,14 @@ export class UserService {
 
   //Appel le back pour générer un mail avec un nouveau mot de passe
   resetPassword(mail: string) {
-    return this.http.post(`${mailEndpoint}/${mail}/password`, mail)
+    return this.http.post(`${mailEndpoint}/${mail}/password`, mail);
   }
 
   //Méthode qui appelle le back pour la modification du mot de passe de l'utilisateur
   updatePassword(password: Password) {
-    return this.http.patch(`${userEndpoint}/${this.currentLoggedUser.value.id}/password`, password)
+    return this.http.patch(
+      `${userEndpoint}/${this.currentLoggedUser.value.id}/password`,
+      password
+    );
   }
 }
-
