4e288ee8da0609207db5cdebfa6da3899b72b5b6
Benjamin Boissin <benjamin.boissin@modiscloud.net>
2023-03-09 10:06:05 +0000

    Merged PR 9244: Task 28459 Modification de la préférences des utilisateurs
    - task: creation du bouton de validation et du valideur de changement de valeurs
    - Task: creation de la méthode qui va appeler le service UserService pour la MaJ des données
    
    Related work items: #28459

--- src/app/user-settings/components/other-settings/other-settings.component.ts
+++ src/app/user-settings/components/other-settings/other-settings.component.ts
@@ -1,7 +1,8 @@
+import { PersonalData } from './../../../users/interfaces/personal-data.model';
 import { Currency } from './../../../currencies/currency-model';
 import { Language } from '../../../languages/interfaces/language.model';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-other-settings',
@@ -13,15 +14,16 @@ export class OtherSettingsComponent implements OnChanges {
   @Input() userLanguage!: string;
   @Input() currencies!: Currency[];
   @Input() userCurrency!: string;
+  @Output("modifyOtherSettings") modificationEvent = new EventEmitter();
   otherSettingsForm!: FormGroup;
 
   constructor(private readonly formBuilder: FormBuilder) { }
 
-
-
   ngOnChanges(changes: SimpleChanges): void {
     if (changes["userLanguage"] || changes["userCurrency"]) {
-      this.initOtherSettingsForm(changes["userLanguage"].currentValue, changes["userCurrency"].currentValue);
+      const userLanguage = changes["userLanguage"] ? changes["userLanguage"].currentValue : this.userLanguage;
+      const userCurrency = changes["userCurrency"] ? changes["userCurrency"].currentValue : this.userCurrency;
+      this.initOtherSettingsForm(userLanguage, userCurrency);
     }
   }
 
@@ -31,12 +33,31 @@ export class OtherSettingsComponent implements OnChanges {
         language_code: [
           codeLanguage, [Validators.required]
         ]
-      ,
+        ,
         currency_code: [
           codeCurrency, [Validators.required]
         ]
-      },
+      }, { validators: [this.noChangeValuesValidator(this.userLanguage, this.userCurrency)] }
     );
   }
+
+  noChangeValuesValidator(userLanguage: string, userCurrency: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const codeLanguage = control.get('language_code');
+      const codeCurrency = control.get('currency_code');
+      return codeLanguage?.value === userLanguage
+        && codeCurrency?.value === userCurrency
+        ? { noChangeValues: true } : null;
+    };
+  }
+
+  //On emet les données au composant parent
+  saveModification() {
+    if (this.otherSettingsForm.valid) {
+      this.modificationEvent.emit(this.otherSettingsForm.value as PersonalData)
+    }
+  }
+
 }
+
 
