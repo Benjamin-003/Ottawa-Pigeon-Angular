84879462c7499415366ff7a030f7a8c0041fcda8
Benjamin Boissin <benjamin.boissin@modiscloud.net>
2023-03-15 11:40:21 +0000

    Merged PR 9418: Task: creation du guard pour protéger l'URL du dashboard
    Task: creation du guard pour protéger l'URL du dashboard
    
    Related work items: #28787

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthentificationGuard } from './authentification/services/authentification-guard.guard';
 import { AccueilComponent } from './core/page/accueil/accueil.component';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
@@ -8,7 +9,7 @@ const routes: Routes = [
   { path: 'tarifs', loadChildren: () => import('./tarifs/tarifs.module').then(m => m.TarifsModule) },
   { path: 'authentification', loadChildren: () => import('./authentification/authentification.module').then(m => m.AuthentificationModule) },
   { path: 'information-societe', loadChildren: () => import('./information-societe/information-societe.module').then(m => m.InformationSocieteModule) },
-  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),canActivate: [AuthentificationGuard] },
   { path: 'parametres', loadChildren: () => import('./user-settings/user-settings.module').then(m => m.UserSettingsModule) },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
 ];
---
+++ src/app/authentification/services/authentification-guard.guard.ts
@@ -0,0 +1,20 @@
+import { UserService } from 'src/app/users/services/user-service.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthentificationGuard implements CanActivate {
+  constructor(private readonly userService: UserService, private readonly route: Router) { }
+  canActivate(): true|UrlTree {
+    if (this.userService.token) {
+      return true
+    }
+    else {
+      return this.route.parseUrl("authentification/connexion")
+    }
+  }
+
+}
