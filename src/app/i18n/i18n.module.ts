963266c4700a1fcbf2d61e686452d74689cb62b8
Benjamin Boissin <benjamin.boissin@modiscloud.net>
2023-04-21 12:39:08 +0000

    Merged PR 9919: Chore: appel du back depuis le module pour récupérer les languages de l'appli...
    Chore: appel du back depuis le module pour récupérer les languages de l'application

--- src/app/i18n/i18n.module.ts
+++ src/app/i18n/i18n.module.ts
@@ -1,26 +1,42 @@
 import { registerLocaleData } from '@angular/common';
 import { APP_INITIALIZER, Injectable, LOCALE_ID } from '@angular/core';
 import { loadTranslations } from '@angular/localize';
+import { LanguagesService } from '../languages/languages.service';
+import { firstValueFrom, map} from 'rxjs';
+import { Language } from '../languages/interfaces/language.model';
 
 @Injectable({
   providedIn: 'root',
 })
-
 class I18n {
   browserLocale = navigator.language;
   locale: string = this.browserLocale.includes('-')
     ? this.browserLocale.substring(0, this.browserLocale.indexOf('-'))
     : this.browserLocale;
   userLocale: string | null = null;
-  async setLocale() {
-    this.userLocale = sessionStorage.getItem('Language');
+  languages: string[] = [];
 
+  constructor(private languageService: LanguagesService) { }
+
+  async setLocale() {
+    this.languages = await firstValueFrom(
+      this.languageService
+        .getLanguages()
+        .pipe(
+          map((tabJson: Language[]) => {
+            return tabJson.map((data: Language) => {
+              return data.code.toLowerCase();
+            });
+          })
+        ))
+
+    this.userLocale = sessionStorage.getItem('Language');
     if (this.userLocale) {
       this.locale = this.userLocale.toLowerCase();
     }
 
-    if (this.locale !== 'fr' && this.locale !== 'en') {
-      this.locale = 'fr'
+    if (this.languages.indexOf(this.locale) === -1) {
+      this.locale = 'fr';
     }
 
     const localeModule = await import(
@@ -39,7 +55,6 @@ class I18n {
     //Charger les traductions pour les paramètres régionaux actuels à la volée
     loadTranslations(localeTranslationsModule.default);
   }
-
 }
 
 //Chargement des données locales au démarrage de l'application
