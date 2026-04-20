c2a5441458a31275e29d78c124ec1b49643eee34
benjamin.boissin <benjamin.boissin>
2026-04-20 10:39:06 +0200

    migration vers augular core 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,51 +6,44 @@ import * as Sentry from "@sentry/angular";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { InputTextModule } from 'primeng/inputtext';
 import { AuthentificationInterceptor } from './authentification/services/authentification-interceptor';
 import { I18nModule } from './i18n/i18n.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    InputTextModule
-  ],
-  providers: [
-    {
-      provide: ErrorHandler,
-      useValue: Sentry.createErrorHandler({
-        showDialog: true,
-      }),
-    },
-    {
-      provide: Sentry.TraceService,
-      deps: [Router],
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: () => () => {
-        // Empty Function
-      },
-      deps: [Sentry.TraceService],
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthentificationInterceptor,
-      multi: true
-    }
-    ,
-    I18nModule.setLocale(),
-    I18nModule.setLocaleId()
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        CoreModule,
+        BrowserAnimationsModule,
+        InputTextModule], providers: [
+        {
+            provide: ErrorHandler,
+            useValue: Sentry.createErrorHandler({
+                showDialog: true,
+            }),
+        },
+        {
+            provide: Sentry.TraceService,
+            deps: [Router],
+        },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: () => () => {
+                // Empty Function
+            },
+            deps: [Sentry.TraceService],
+            multi: true,
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthentificationInterceptor,
+            multi: true
+        },
+        I18nModule.setLocale(),
+        I18nModule.setLocaleId(),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
