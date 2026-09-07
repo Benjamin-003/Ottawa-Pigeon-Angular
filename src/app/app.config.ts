ce04a456cd109dd4fd3a3fb572214b5728ff3a44
benjamin.boissin <benjamin.boissin>
2026-09-07 09:16:54 +0200

    deletion of auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,6 @@ import { routes } from './app.routes';
 import { authInterceptor } from './core/auth/auth.interceptor';
 import { AuthService } from './core/auth/auth.service';
 import { I18nService } from './i18n/i18n.service';
-import { Router } from '@angular/router';
-import { ErrorHandler } from '@angular/core';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { definePreset } from '@primeng/themes';
